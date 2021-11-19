9e5b532ffddbb92e9313e8c833afb2e1cc3b8e3e
Brian Luong <bhluong1codes@gmail.com>
2021-11-18 20:28:52 -0800

    Updated dom-event-delegation

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,10 +1,12 @@
 var $taskList = document.querySelector('.task-list');
+
 $taskList.addEventListener('click', function (event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName', event.target.tagName);
-  var ancestor = event.target.closest('.task-list-item');
-  console.log('closest', ancestor);
+
   if (event.target.tagName === 'BUTTON') {
-    event.target.closest('.task-list-item').remove();
+    var ancestor = event.target.closest('.task-list-item');
+    console.log('closest', ancestor);
+    ancestor.remove();
   }
 });
